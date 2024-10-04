0acfdd8410404e31292feaacf38e685b79c83365
AdarshKhare29 <adarshkhare86@gmail.com>
2024-10-04 20:03:21 +0530

    Added test case for single input and handle multiple input

--- src/hooks/useCalculator.ts
+++ src/hooks/useCalculator.ts
@@ -1,7 +1,13 @@
 function add(numbers: string): number {
     if (numbers === "") return 0;
-
-    return 0
+    let delimiters = /,|\n/; // Default delimiters: comma or newline
+    const numArray = numbers.split(delimiters).map((num) => {
+        const parsedNum = parseFloat(num.trim());
+        return isNaN(parsedNum) ? 0 : parsedNum;
+    });
+    console.log('numArray: ', numArray)
+    return numArray
+        .reduce((sum, n) => sum + n, 0);
 }
 
 export { add };
--- src/tests/StringCalculator.test.tsx
+++ src/tests/StringCalculator.test.tsx
@@ -4,4 +4,14 @@ describe("String Calculator", () => {
     test("should return 0 for an empty string", () => {
         expect(add("")).toBe(0);
     });
+    test("should return the number itself for a single number", () => {
+        expect(add("1")).toBe(1);
+    });
+    test("should return the sum of two comma-separated numbers", () => {
+        expect(add("1,5")).toBe(6);
+    });
+
+    test("should handle an unknown number of comma-separated numbers", () => {
+        expect(add("1,2,3,4")).toBe(10);
+    });
 });
